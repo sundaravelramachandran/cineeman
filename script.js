b1ed88809a65826a01cb5238fd19568490432b28
sundar <sundaravelramachandran@gmail.com>
2025-07-04 11:15:14 +0530

    adding all files

---
+++ script.js
@@ -0,0 +1,236 @@
+movie_details=[{moviename:"arm",
+    link:"https://qwe3.viidshar.com/embed-pne77tirzkxi.html",
+    story:"The movie begins with a grandmother telling a bed time story to her granddaughter the lore of Chyothivilaku. Chyothivilaku is a sacred lamp which was made from a melted mixture of an asteroid which once crashed in the land of Haripuram in the ancient times. The lamp was commanded to be made from the asteroid by the Edakkal King at the time, who takes it to his palace owing to its supernatural powers",
+    language:"Tamil",
+    img_name:'arm.jpg',
+    rating:"IMDb 4.5",
+    director_name:"Jithin Lal",
+    director_movies:"Kalki (2019) and Maravairi (2020)",
+    genere:"Action · Adventure · Comedy · Drama · Fantasy",
+    cast:"Tovino Thomas.Sreerang.Maniyan.Neeliyathu Kunjikelu.Krithi Shetty.Anvi.Basil Joseph.Aishwarya Rajesh.Surabhi Lakshmi.Rohini.Harish Uthaman.Jagadish.Madhupal.Shivajith.Aju Varghese.Nisthar Sait.Shivaraj.Pramod Shetty.Sudheesh.Sanju Sivram.Biju Kuttan.Jitin Puthenchery.Nihal.Nirmal Palazhi.Aneesh Gopal.Santhosh Keezhattoor.Parvathi T..Kabir Duhan Singh.Jithin Laal.Mohanlal.Vikram.Shiva Rajkumar"
+},{
+    link:"https://qwe3.viidshar.com/embed-xwer247zw4q4.html",
+    moviename:"dragon",
+    story:"In 2014, D. Ragavan, a diligent 12th-standard student, wins a gold medal in computer science. Encouraged by his achievement, he confesses his feelings to his crush, Anjana, but she rejects him, preferring rebellious boys over well-mannered ones like him. Heartbroken, Ragavan transforms into a rebellious student, adopting the moniker 'Dragon'. With his friend Anbu, he builds a notorious reputation at AGS Engineering College in Vellore, accumulating 48 arrears. Despite his poor academics, he earns the respect of his peers",
+    language:"Tamil",
+    img_name:'dragon.webp',
+    rating:"IMDb 4.5",
+    director_name:" Ashwath Marimuthu",
+    director_movies:"Oh My Kadavule(2020) ",
+    genere:" Drama · Fantasy",
+    cast:"Pradeep Ranganathan.Anupama Parameswaran.Kayadu Lohar.Mysskin.Gautham Vasudev Menon.K. S. Ravikumar.George Maryan.Indumathy Manikandan.VJ Siddhu.Harshath Khan.Ivana.Sneha.Gopika Ramesh.Anveshi Jain.P. L. Thenappan.Bjorn Surrao.Ravindhar Chandrasekaran.Apollo Ravi.Joe Michael Praveen.Whatsapp Mani.Ramesh Narayanan.Lakshman Tekumudi.Rohith Muralidharan.Deepa Shankar.Varshini Karmegam.Sujatha Babu.Aruljothi Arockiaraj.Ashwath Marimuthu.Kaushik Gabriell"
+}
+,{
+    link:"https://qwe3.viidshar.com/embed-xlzgch5y20zm.html",
+    moviename:"luckybaskhar",
+    story:"One morning in 1992 Bombay, Baskhar Kumar is suddenly taken by CBI officials to the Colaba branch of Magadha Bank for interrogation about his alleged embezzlement of its funds during his employment to conduct securities fraud. The real story is revealed through various flashbacks featuring Baskhar breaking the fourth wall regularly to provide additional commentary. Three years earlier, Baskhar is a cashier in Magadha Bank, one of the few remaining private banks in India following nationalisation. ",
+    language:"Tamil",
+    img_name:'luckybaskhar.jpg',
+    rating:"IMDb 4.6",
+    director_name:" Venky Atluri",
+    director_movies:"Oh My Kadavule(2020) ",
+    genere:" crime, drama, and thriller",
+    cast:"Dulquer Salmaan.Meenakshi Chaudhary.Tinnu Anand.P. Sai Kumar.Ramki.Raghu Babu.Maanasa Choudhary.Sarvadaman D. Banerjee.Kalyani.Sachin Khedekar.Kasi Viswanath.Sudha.Rajkumar Kasireddy.Prabhas Sreenu.Hyper Aadi.Sivannarayana Naripeddi.Gayatri Bhargavi.Mathew Varghese.Srinath Maganti.Micky Makhija.Srikanth Reddy.Manava Koteshwar Rao.Lab Sharath.Ram Sandeep Varma Jampana.Anji Chodapaneedi.Rahul.Mahesh Achanta.Manik Reddy.Mukhtar Khan.S. P. Prabhu.Kishore Raju Vasistha.Krishna Kamal.Shashidhar.Ananya Sharma.Rithvik.Charan Lakkaraju.Surya Sreenivas.Jayasri Rachakonda"
+}
+
+,{
+    link:"https://qwe3.viidshar.com/embed-ihm3q7aynk6n.html",
+    moviename:"empuraan",
+    story:"In 2002, communal riots break out in Gujarat between Hindu and Muslim extremists, forcing a young Zayed Masood and his family to flee. They seek refuge with Subhadra Ben, a kind lady and the head of a large royal family, who entrusts their safety with her nephew, Munna. However, Munna, harbouring a deep hatred towards Muslims, informs his elder brother, Balraj Patel, who is a Hindu extremist leader. Balraj leads a mob that massacres Zayed's family, including his father Masood, and mother Bahija Begum.",
+    language:"Tamil",
+    
+    img_name:'empuraan.jpg',
+    rating:"IMDb 4.6",
+    director_name:" Prithviraj Sukumaran",
+    director_movies:"Bro Daddy(2022),Lucifer(2019) ",
+    genere:" Action, Crime, Thriller",
+    cast:"Mohanlal. Pranav Mohanlal. Adarsh. Prithviraj Sukumaran. Karthikeya Dev. Abhimanyu Singh. Tovino Thomas. Manju Warrier. Indrajith Sukumaran. Jerome Flynn. Andrea Tivadar. Saikumar. Baiju Santhosh. Suraj Venjaramoodu. Kishore. Sukant Goel. Eriq Ebouaney. Fazil. Sachin Khedekar. Nandhu. Shivaji Guruvayoor. Aneesh G. Menon. Manikuttan. Saniya Iyappan. Nyla Usha. Giju John. Murugan Martin. Sshivada. Nikhat Khan. Alexx O'Nell. Mikhail Novikov. Satyajit Sharma. Oziel Jivani. Aishwarya Ohja. Nayan Bhatt. Shubhangi Latkar. Behzaad Khan. Shaun Romy. Jaise Jose. Kalabhavan Shajohn. Antony Perumbavoor. Aashish Joe Antony. Rick Yune."
+}
+,{
+    link:"https://qwe3.viidshar.com/embed-wdxnydyow3a9.html",
+    moviename:"ddnextlevel",
+    story:"Krishnamoorthy Kissa is a sharp-tongued YouTube film reviewer known for his brutally honest critiques. One day, he receives an invitation from Hitchcock Iruthyaraj, a mysterious filmmaker, to attend a private screening of his latest horror-comedy film, DD Next Level. Kissa, thinking it’s just another publicity stunt, agrees to go—bringing along his family and girlfriend, Aasai Harshini.",
+    language:"Tamil",
+
+    img_name:'dd-next.webp',
+    rating:"IMDb 3.5",
+    director_name:" S. Prem Anand",
+    director_movies:"Bro Daddy(2022),Lucifer(2019) ",
+    genere:" Comedy, Horror, Thriller",
+    cast:"Santhanam. Selvaraghavan. Gautham Vasudev Menon. Geethika Tiwary. Ashmini Balakrishnan. Nizhalgal Ravi. Kasthuri Shankar. Shilpa. Yashika Aannand. Rajendran. Lollu Sabha Maaran. Redin Kingsley."
+},
+{
+    link:"https://qwe3.viidshar.com/embed-typ4ad4xu7q3.html",
+    moviename:"retro",
+    story:"In 1960s Thoothukudi, a gangster’s wife, Sandhya, adopts an orphaned infant with a spear-shaped scar on his stomach. She names him Paarivel Paari Kannan, raising him lovingly while her husband, Thilagan, refuses to acknowledge the boy. After Sandhya’s death, a teenage Paari meets Rukmini at their mothers’ funeral rites in Varanasi. On their train back to Thoothukudi, when Thilagan is ambushed, Paari saves him, earning Thilagan’s reluctant respect.",
+    language:"Tamil",
+    img_name:'retro.jpeg',
+    rating:"IMDb 3.5",
+    director_name:"Karthik Subbaraj",
+    director_movies:"Petta,Jigarthanda,Mahaan,etc",
+    genere:" Romance/Adventure",
+    cast:"Suriya. Harish. Nithran.Sai. Pooja.Hegde. Aavni. Joju.George. Jayaram. Nassar. Prakash.Raj. Vidhu. Sanjay. Karunakaran. Gajaraj. Swasika. Avinash.Raghudevan. Rakesh.Rakku. Kumar.Natarajan. Kaarthekeyen.Santhanam. Thamizh. Sujith.Shankar. Muthukumar. Singampuli. Sathya. Rajsekar.Karpoorasundarapandian. Vijay.Muthu. Prem.Kumar. Sandeep.Raj. Ramachandran.Durairaj. MD.Asif. Arul. Murugavel. Muthupandii. Manoj. Yamuna. Nithish. Thirshiv. Sanjay. Remya.Suresh. Sabitha.Roy. Sai.Dheena. Seemon.Abbas. Mohan.Das. Aranganathan. Ashokraja. Sooraj. Manoj. Merlin. Sathuryan. Selvakumar. Jeeva.Pandi. Vinoth. Alavudeen. Gana.Kamarah. Vetri.Gopi.GPR. Ezhil. Santhosh.Kanna. Naveen. Hari.Krishna. Surendhar. Sai.Dinesh. Giridharaja. Shriya.Saran. Santhosh.Narayanan"
+},
+{
+    link:"https://qwe3.viidshar.com/embed-1mzh59325ngb.html",
+    moviename:"vazha",
+    story:"Five happy-go-lucky teenage boys are deemed vaazhas (a slang for good-for-nothing) by their overbearing parents for not meeting their expectations. Their emotional tale sees them embark on a journey of self-discovery and acceptance, where love ultimately triumphs over judgment.What they achieve in life forms the remaining part of the story.",
+    language:"Malayalam",
+    img_name:'vaazha.jpg',
+    rating:"IMDb 4.5",
+    director_name:"	Anand Menen",
+    director_movies:"Gauthamante Radham",
+    genere:" comedy-drama",
+    cast:"Siju.Sunny. Amith.Mohan.Rajeshwari. Joemon.Jyothir. Anuraj.O.B. Anshid.Anu. Saaf. Basil.Joseph. Jagadish. Azees.Nedumangad. Kottayam.Nazeer. Noby.Marcose. Arunsol. Gibin.Gopinath. Sminu.Sijo. Priya.Sreejith. Meenakshi.Unnikrishnan. Sruthy.Manikandan. Hashir. Gouri.Sankar"
+},
+{
+    link:"https://qwe3.viidshar.com/embed-t7y7fjwrjx64.html",
+    moviename:"sookshmadarshini",
+    story:"Priyadarshini alias Priya, a homemaker on a desperate job-hunting, lives with her husband Anthony and their daughter Kani in a close-knit community. Priya and her neighbour Stephy, a single mother, do well in their technical interviews for the same company; concurrently, Manuel, who runs his father's business Grace Bakers, moves into their neighborhood with his widowed aged mother Grace. Though Manuel gets along well with all the neighbours, Priya believes that he is rather a shady person. Her suspicions indeed turn out to be true when he kills an monitor lizard and feeds it to his neighbours in a party under the guise of beef. His intention is to commit a crime, with the help of his uncle Roy and his cousin Dr. John while Grace remains cloistered and avoids communication with any of the neighbours.",
+    language:"Malayalam",
+    img_name:'sookshmadarshini.avif',
+    rating:"IMDb 4.6",
+    director_name:"	M. C. Jithin",
+    director_movies:"Nonsense (2018)",
+    genere:"  Thriller/Mystery",
+    cast:"Nazriya.Nazim. Basil.Joseph. Akhila.Bhargavan. Merin.Philip. Pooja.Mohanraj. Sidharth.Bharathan. Deepak.Parambol. Kottayam.Ramesh. Manohari.Joy. Abhiram.Radhakrishnan. Hezzah.Mehak. Janani.Ram. Saraswathi.Menon. Gopan.Mangattu. Jaya.Kurup. Rini.Udayakumar. James. Noushad.Ali. AV.Anoop. Athira.Rajeev. Sajeevan. Sajeed. Binni.Rinki. Nandan.Unni. Jossy.Sijo. Fr.Saji. Ajmal.Shah. Dhyan.Diva"
+},
+{
+    link:"https://qwe3.viidshar.com/embed-2ug3l7pdjdup.html",
+    moviename:"thuglife",
+    story:"Gang leader Sadanand Yadav betrays a truce by tipping off police, sparking a violent shootout. Amidst the chaos, a newspaper vendor is killed—his two young children, Amaran and Chandra, are separated. Sakthivel mistakenly believes Amaran saved him and adopts him, raising the boy and promising to find Chandra later",
+    language:"Tamil",
+    img_name:'thuglife.jpg',
+    rating:"IMDb  2.3",
+    director_name:"	Mani Ratnam",
+    director_movies:"Nayakan (1987),'​Roja' (1992),'Bombay' (1995),​'Anjali' (1990)",
+    genere:"Action Thriller / Crime Drama",
+    cast:"Kamal Haasan. Silambarasan TR. Trisha Krishnan. Aishwarya Lekshmi. Ashok Selvan. Abhirami. Joju George. Nassar. Mahesh Manjrekar. Ali Fazal. Tanikella Bharani. Vadivukkarasi. Sanjana Krishnamoorthy. Vaiyapuri. Arjun Chidambaram. Chetan. Bagavathi Perumal. Rohit Saraf. Chinni Jayanth. Baburaj. Gopi Kannadasan. Elango Kumaravel. Nithya Shri. Srikanth K. Vijayan. Mahitha Ravilla. Adithya Krishnan R. Alisha Rahmam. Smrithi Parameswar. Lavi Pajni. Jelaja. Rajshri Deshpande. Sanya Malhotra"
+},
+{
+  link: "https://qwe3.viidshar.com/embed-ih0mzamgw2vn.html",
+  moviename: "Good Bad Ugly",
+  story: "After reforming and serving time, former gangster AK (Ajith Kumar) seeks a quiet life with his family. But when his son is wrongfully jailed, AK is forced back into his violent past to save him.",
+  language: "Tamil",
+  img_name: "good_bad_ugly.jpg",
+  rating: "IMDb 6.5/10 (viewer average); Critics ~3.5/5",
+  director_name: "Adhik Ravichandran",
+  director_movies: "Trisha Illana Nayanthara (2015), Anbanavan Asaradhavan Adangadhavan (2017), Bagheera (2023)",
+  genre: "Action Comedy / Mass Thriller",
+  cast: "Ajith Kumar (AK), Trisha Krishnan, Arjun Das, Sunil, Prabhu, Prasanna, Yogi Babu, Simran (cameo), Jackie Shroff, Priya Prakash Varrier"
+}
+,
+{
+  link: "https://qwe3.viidshar.com/embed-qihztyzcf3e3.html",
+  moviename: "Veera Dheera Sooran: Part 2",
+  story: "Kaali, a provision store owner in Madurai, leads a peaceful life with his wife and children. When the past catches up, Kaali is forced to take one last hit job over a fateful festival night, pulling him into a dangerous crime network.",
+  language: "Tamil",
+  img_name: "veera_dheera_sooran.jpg",
+  rating: "IMDb 7.5",
+  director_name: "S. U. Arun Kumar",
+  director_movies: "Pannaiyarum Padminiyum (2014), Sethupathi (2016), Chithha (2023)",
+  genere: "Action Thriller / Crime Drama",
+  cast: "Vikram (Kaali), S. J. Suryah, Suraj Venjaramoodu, Dushara Vijayan, Siddique, Kalaiyarasan"
+}
+
+
+
+
+];
+{/* <IFRAME SRC="https://qwe3.viidshar.com/embed-ih0mzamgw2vn.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=640 HEIGHT=360 allowfullscreen></IFRAME> */}
+{/* <IFRAME SRC="https://qwe3.viidshar.com/embed-qihztyzcf3e3.html" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=640 HEIGHT=360 allowfullscreen></IFRAME> */}
+
+function moviePage(moviename){
+    let moviename1=moviename;
+    let found=false
+    for(let i=0;i<movie_details.length;i++){
+        if(moviename==movie_details[i].moviename){
+            localStorage.setItem("movie_Data",JSON.stringify(movie_details[i])) ;
+            window.location.href="viewmovie.html"
+            found=true;
+            console.log(movie_details[i]);
+        }
+        else{
+            console.log(moviename);
+            
+        }
+       
+
+    }
+    if(found==false){
+        alert("The movie you are searching is not available: " + moviename1);
+    }
+}
+trailer_list=[
+    {name:"jananayagan",
+    link:"https://www.youtube.com/watch?v=MKUDHKf_pkg&list=RDMKUDHKf_pkg&start_radio=1"
+},
+{
+    name:"kubera",
+    link:"https://www.youtube.com/watch?v=Eyl4sQFkQiM"
+},{
+    name:"coolie",
+    link:"https://www.youtube.com/watch?v=EptnMYijPjc&list=RDEptnMYijPjc&start_radio=1"
+},
+{
+    name:"kiss",
+    link:"https://www.youtube.com/watch?v=g9_CSz_HeWk"
+}];
+function movieTrailer(trailername){
+    let found=false
+    for (let i=0;i<trailer_list.length;i++){
+        if (trailername==trailer_list[i].name){
+            // console.log(trailer_list[i])
+            alert("The trailer will play in youtube")
+            window.open(trailer_list[i].link,"_blank");
+            found=true;
+        }
+    
+
+    }
+    if (found==false){
+        alert("Movie Not Found ");
+    }
+}
+// function Find(){
+//     let found=false;
+
+//     let userInput=document.getElementById('userinput');
+//     for (let i=0;i<movie_details.length;i++){
+//         if (userInput.value==movie_details[i].moviename){
+//             // console.log(movie_details[i]);
+//             localStorage.setItem("searched_movie",JSON.stringify(movie_details[i]));
+//             window.location.href="find.html";
+//             found=true;
+            
+
+
+//         }
+
+//     }
+//     if(found==false){
+//         alert("movie not Found")
+//     }
+
+
+// }/
+function Find(){
+    let found=false;
+    let userInput=document.getElementById('userinput');
+    let match=[];
+    // let trailerMatch=[];
+    for (let i=0;i<movie_details.length;i++){
+        if ((movie_details[i].moviename.toLowerCase().startsWith(userInput.value.toLowerCase()))){
+            
+            match.push(movie_details[i].moviename);
+            localStorage.setItem('match_list',JSON.stringify(match));
+            window.location.href='find.html'
+            found=true;
+        }
+
+    }
+    if (found==false){
+            alert("Movie not found")
+    }
+}
+
